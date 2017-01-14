f4516568a49131a9e2453171fdf92c1ed4300ba5
ViksataK <viktor.klisurov@gmail.com>
2017-01-14 17:08:59 +0200

    correct solutions commit

--- tasks/1/solution.rb
+++ tasks/1/solution.rb
@@ -5,6 +5,7 @@ TABLE_ = {
   'silver' => [961.8, 2162],
   'copper' => [1085, 2567]
 }
+
 def convert_between_temperature_units(deg, from_unit, to_unit)
   if from_unit != to_unit
     if from_unit == 'C'
@@ -18,14 +19,15 @@ def convert_between_temperature_units(deg, from_unit, to_unit)
     deg
   end
 end
+
 def melting_point_of_substance(subst, unit)
-  
   if unit == 'C'
     TABLE_[subst][0]
   else
     convert_between_temperature_units(TABLE_[subst][0], 'C', unit)
   end
 end
+
 def boiling_point_of_substance(subst, unit)
   if unit == 'C'
     TABLE_[subst][1]
@@ -33,6 +35,7 @@ def boiling_point_of_substance(subst, unit)
     convert_between_temperature_units(TABLE_[subst][1], 'C', unit)
   end
 end
+
 def from_celsius(deg, to_unit)
   if to_unit == 'K'
     deg + 273.15
@@ -42,6 +45,7 @@ def from_celsius(deg, to_unit)
     'Bad units'
   end
 end
+
 def from_kelvins(deg, to_unit)
   if to_unit == 'C'
     deg - 273.15
@@ -49,11 +53,11 @@ def from_kelvins(deg, to_unit)
     deg.to_f * 9 / 5 - 459.67
   end   
 end
+
 def from_farenheit(deg, to_unit)
   if to_unit == 'K'
     (deg.to_f + 459.67) * 5 / 9
   elsif to_unit == 'C'
     (deg.to_f - 32) * 5 / 9
-+
   end
 end
--- tasks/4/solution.rb
+++ tasks/4/solution.rb
@@ -28,6 +28,7 @@ RSpec.describe 'Version' do
       v2 = Version.new('3.2.2')
       expect(v1 > v2).to be false
     end
+
     it "v1 == v2 is true when v1 == v2" do
       v1 = Version.new('2.3.2')
       v2 = Version.new('2.3.2')
@@ -49,28 +50,31 @@ RSpec.describe 'Version' do
       expect(v1 != v2).to be false
     end
   end
+
   describe '#to_s' do
     it 'return good string' do
       expect(Version.new('4.5.2').to_s).to eq '4.5.2'
     end
   end
+
   describe '#components' do
     it "return good array when called without argument" do
       expect(Version.new('4.5.7').components).to eq [4, 5, 7]
     end
     it "return good array when called without argument and 
-    last component is 0"do
+    last component is 0" do
       expect(Version.new('0.4.0').components).to eq [0, 4]
     end
     it "return good array when called with argument" do
       expect(Version.new('2.4.1').components(2)).to eq [2, 4]
     end
     it 'return good array when called with argument 1 and
-    second component is 0'do
+    second component is 0' do
       expect(Version.new('4.0.0').components(1)).to eq [4]
     end
   end
 end
+
 RSpec.describe "Version::Range" do
   describe '#include?' do
     before do
@@ -95,7 +99,6 @@ RSpec.describe "Version::Range" do
   describe '#to_a' do
     it 'return good array' do
       expect(Version::Range.new('1.9.7', '2.0.1').to_a)
-+
               .to eq ['1.9.7', '1.9.8', '1.9.9', '2']
     end
     it 'return empty array when start_version>end_version' do
